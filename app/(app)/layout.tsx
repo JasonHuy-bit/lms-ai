924d7c782c260d12c55460659c9311a04d1c2728
JasonHuy-bit <huymepro689@gmail.com>
2026-04-07 11:19:43 +0700

    Add: Vercel Analytics to layout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,6 +1,8 @@
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TutorWidget } from "@/components/tutor";
+import {Analytics} from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,6 +10,8 @@ function AppLayout({ children }: { children: React.ReactNode }) {
       <div>{children}</div>
       <SanityLive />
       <TutorWidget />
+      <Analytics />
+      <SpeedInsights />
     </ClerkProvider>
   );
 }
